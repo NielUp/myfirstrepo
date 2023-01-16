2856fe46c7a776f837e45d544d60ef945473ffe7
Niels Uphoff <nielsuphoff@gmail.com>
2023-01-16 09:46:57 +0100

    initial commit

---
+++ src/main/java/dev/courses/springdemo/AssignmentDirectory/Injection.java
@@ -0,0 +1,9 @@
+package dev.courses.springdemo.AssignmentDirectory;
+
+public class Injection {
+
+    public static void main(String[] args) {
+        // call OutputAggregator.printInput("Hello world") method with an input
+    }
+
+}
